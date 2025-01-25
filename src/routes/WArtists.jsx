5042bf0e9b2f9a36f77eb04dc4047f275b8261fa
Emmanuel A. <alabiemmanuel2937@gmail.com>
2025-01-25 15:14:57 +0000

    ...

--- src/routes/WArtists.jsx
+++ src/routes/WArtists.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 
 export default function WArtists() {
 
-    const images = import.meta.glob('/i/artists/*');
-    const imagePaths = Object.keys(images);
-    
+    const images = import.meta.glob("/src/assets/artists/*", { eager: true }); // Eagerly load images
+  const imagePaths = Object.values(images).map((mod) => mod.default); // Extract paths
 
     return (
         <>
